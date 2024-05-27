93684c78954ffc453ba345aec07b9a008775b7be
Alexis Poaquiza <106999645+AlexiP18@users.noreply.github.com>
2024-05-27 18:34:01 +0000

    Se establecio la creación de usuarios mediante firebase y se modificado sign-up

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth'
-import { getAuth,signInWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, updateProfile} from 'firebase/auth';
 import { User } from '../models/user.model';
 
 @Injectable({
@@ -10,10 +10,20 @@ export class FirebaseService {
   auth = inject(AngularFireAuth);
 
 
-  // Autenticación
+  //**************Autenticación
 
   // Acceder
-  signIn(user: User){
-    return signInWithEmailAndPassword(getAuth(), user.email,user.password)
+  signIn(user: User) {
+    return signInWithEmailAndPassword(getAuth(), user.email, user.password)
+  }
+
+  //Crear Usuario
+  signUp(user: User) {
+    return createUserWithEmailAndPassword(getAuth(), user.email, user.password)
+  }
+
+  //Actualizar Usuario
+  updateUser(displayName:string){
+    return updateProfile(getAuth().currentUser,{displayName})
   }
 }
