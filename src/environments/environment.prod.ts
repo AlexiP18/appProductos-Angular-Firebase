dd1ca3c0baa1be3782cbd65577008fd52c7ab4b0
Alexis Poaquiza <apoaquiza6287@uta.edu.ec>
2024-05-27 11:46:21 -0500

    Creación de módulo shared, creacion de componente header y login e integración con firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCFoVAVzQ67gPhNSrSKhPoiu47DIT0C95o",
+    authDomain: "product-admin-app-129a0.firebaseapp.com",
+    projectId: "product-admin-app-129a0",
+    storageBucket: "product-admin-app-129a0.appspot.com",
+    messagingSenderId: "409804279950",
+    appId: "1:409804279950:web:34c909d1bf990d8ee8b9df"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCFoVAVzQ67gPhNSrSKhPoiu47DIT0C95o",
+    authDomain: "product-admin-app-129a0.firebaseapp.com",
+    projectId: "product-admin-app-129a0",
+    storageBucket: "product-admin-app-129a0.appspot.com",
+    messagingSenderId: "409804279950",
+    appId: "1:409804279950:web:34c909d1bf990d8ee8b9df"
+  }
 };
 
 /*
