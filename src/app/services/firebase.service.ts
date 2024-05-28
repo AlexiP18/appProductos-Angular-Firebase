83f55b311922f5d25bfac6985a34dd7646b38d94
Alexis Poaquiza <106999645+AlexiP18@users.noreply.github.com>
2024-05-28 03:14:08 +0000

    Obtener datos del usuario en el Login y enrutamiento al /home  en auth y firebase.service

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,9 +1,9 @@
 import { Injectable, inject } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth'
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, updateProfile} from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, updateProfile, sendPasswordResetEmail} from 'firebase/auth';
 import { User } from '../models/user.model';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { getFirestore, setDoc, doc } from '@angular/fire/firestore';
+import { getFirestore, setDoc, doc, getDoc} from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -30,11 +30,20 @@ export class FirebaseService {
   updateUser(displayName:string){
     return updateProfile(getAuth().currentUser,{displayName})
   }
+  //Enviar email para restablecer contraseña
+  sendRecovery(){
+    
+  }
 
   //************Base de Datos
 
   //Setear un documento
   setDocument(path:string, data: any){
     return setDoc(doc(getFirestore(), path),data);
+  }
+
+  //Setear un documento
+  async getDocument(path:string){
+    return (await getDoc(doc(getFirestore(), path))).data();
   }
 }
