b9091b4b80c643104784fbb68e39599a60e16585
Alexis Poaquiza <106999645+AlexiP18@users.noreply.github.com>
2024-05-28 14:50:33 +0000

    Se establecio la función para crear-actualizar productos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FirebaseService } from '../services/firebase.service';
+import { UtilsService } from '../services/utils.service';
+import { AddUpdateProductComponent } from '../shared/components/add-update-product/add-update-product.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+  firebaseSvc = inject(FirebaseService);
+  utilsSvc = inject(UtilsService);
+
+  ngOnInit(){
+
+  }
+
+  //Cerrar Sesión
+  signOut(){
+    this.firebaseSvc.signOut();
+  }
+
+  //Agregar o Actualizar producto
+  addUpdateProduct(){
+    this.utilsSvc.presentModal({
+      component: AddUpdateProductComponent
+    })
+  }
 
 }
